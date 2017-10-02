c914756568ffa44734b2bc059d66f23eab93e032
Likid Geimfari <likid.geimfari@gmail.com>
2017-10-02 08:51:22 +0300

    Updated short description of project

--- wasserstoff/__version__.py
+++ wasserstoff/__version__.py
@@ -2,7 +2,7 @@
 
 __version__ = '0.0.4'
 __title__ = 'wasserstoff'
-__description__ = 'Store your configurations for applications in JSON, XML, INI and txt.'
+__description__ = 'Store your configurations in JSON, XML, INI and txt.'
 __url__ = 'https://github.com/lk-geimfari/wasserstoff'
 __author__ = 'Likid Geimfari (Isaak Uchakaev)'
 __author_email__ = 'likid.geimfari@gmail.com'
